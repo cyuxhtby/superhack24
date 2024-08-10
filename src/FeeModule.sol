d9fce5badff398c3d861613e62e0618b0a43fc08
Diego Aguilar <d.aguilaro99@gmail.com>
2024-08-10 13:47:14 -0500

    adds _calculateDynamicFee

--- src/FeeModule.sol
+++ src/FeeModule.sol
@@ -2,14 +2,15 @@
 pragma solidity ^0.8.19;
 
 import { ISwapFeeModule, SwapFeeModuleData } from 'valantis-core/swap-fee-modules/interfaces/ISwapFeeModule.sol';
-import { IVault } from './interfaces//IVault.sol';
 import { IERC20 } from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import { IVault } from './interfaces//IVault.sol';
 
-
-/// @notice Dynamic swap fee module based on asset balances and virtual reserves in the vault.
+/// @notice Dynamic swap fee module based on asset balances and market weighted reserves in the vault.
 contract FeeModule is ISwapFeeModule {
     IVault public vault;
-    uint256 public constant BASE_FEE_BIPS = 3; // 0.03% base fee in bips
+    uint256 public constant BASE_FEE_BIPS = 10; // 0.1%
+    uint256 public constant FEE_COEFFICIENT_BIPS = 3; // 0.03%
+    uint256 constant PRECISION = 1e18;
 
     constructor(address _vault) {
         vault = IVault(_vault);
@@ -28,15 +29,44 @@ contract FeeModule is ISwapFeeModule {
             internalContext: new bytes(0)
         });
     }
+
+    function callbackOnSwapEnd(
+        uint256 _effectiveFee,
+        int24 _spotPriceTick,
+        uint256 _amountInUsed,
+        uint256 _amountOut,
+        SwapFeeModuleData memory _swapFeeModuleData
+    ) external override {}
+
+    function callbackOnSwapEnd(
+        uint256 _effectiveFee,
+        uint256 _amountInUsed,
+        uint256 _amountOut,
+        SwapFeeModuleData memory _swapFeeModuleData
+    ) external override {}
  
-    // (tokenIn.balance / tokenOut.balance) - oraclePrice(tokenIn/tokenOut)
-    // The fee amount is deduced from tokenOut.
+    // Calculates dynamic fee from market-weighted reserves and ensures it's not below the base fee.
     // Majority asset gives higher fees, and lower fees for minority asset.
-    // Final fee is expressed in basis points 
     function _calculateDynamicFee(
         address tokenIn,
         address tokenOut,
         uint256 amountIn
     ) internal view returns (uint256) {
+        address pool = msg.sender;
+        address[] memory tokens = new address[](2);
+        tokens[0] = tokenIn;
+        tokens[1] = tokenOut;
+        uint256[] memory reserves = vault.getReservesForPool(pool, tokens);
+        uint256 reserveIn = reserves[0];
+        uint256 reserveOut = reserves[1];
+
+        uint256 balanceRatio = (reserveIn * PRECISION) / reserveOut; 
+        uint256 feeInBips = BASE_FEE_BIPS + (FEE_COEFFICIENT_BIPS * (balanceRatio - PRECISION)) / PRECISION;
+
+        if (feeInBips < BASE_FEE_BIPS) {
+            feeInBips = BASE_FEE_BIPS;
+        }
+
+        return feeInBips;
     }
 }
